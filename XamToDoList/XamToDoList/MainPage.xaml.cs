5b8cf06d91fafc89399175e0381318ae9523e1e6
Хамелеон <maxali946@gmail.com>
2018-07-24 12:06:36 +0400

    Add project files.

---
+++ XamToDoList/XamToDoList/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> ToDoList { get; set; }
+
+        private object listSelectedItem;
+        public object ListSelectedItem
+        {
+            get { return listSelectedItem; }
+            set { listSelectedItem = value; OnPropertyChanged(); ((Command)DeleteItem).ChangeCanExecute(); }
+        }
+
+
+        private string toDoItem = "";
+        public string ToDoItem
+        {
+            get { return toDoItem; }
+            set { toDoItem = value; OnPropertyChanged(); ((Command)AddItem).ChangeCanExecute(); }
+        }
+
+        private ICommand addItem;
+        public ICommand AddItem
+        {
+            get
+            {
+                if (addItem is null)
+                {
+                    addItem = new Command(
+                        () =>
+                        {
+                            ToDoList.Add(ToDoItem);
+                        },
+                        () =>
+                        {
+                            if (ToDoItem == "")
+                                return false;
+                            else
+                                return true;
+
+                        });
+                }
+
+                return addItem;
+            }
+        }
+
+        private ICommand deleteItem;
+        public ICommand DeleteItem
+        {
+            get
+            {
+                if (deleteItem is null)
+                {
+                    deleteItem = new Command(
+                        () =>
+                        {
+                            ToDoList.Remove(ListSelectedItem.ToString());
+                            ListSelectedItem = null;
+                            ((Command)DeleteItem).ChangeCanExecute();
+                        },
+                        () => 
+                        {
+                            if (ListSelectedItem == null)
+                                return false;
+                            else
+                                return true;
+                        }); 
+                }
+
+                return deleteItem;
+            }
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ToDoList = new ObservableCollection<string>();
+            BindingContext = this;
+        }
+    }
+}
